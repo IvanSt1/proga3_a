72d5f19937e310246c491581e0d3adcc4563b53b
IvanSt1 <ivanstupnikov4@gmail.com>
2021-04-12 18:04:35 +0300

    a

--- table.c
+++ table.c
@@ -8,48 +8,49 @@
 #include "item.h"
 
 
-Item * find(Table *ptab,int k1, char *k2){
+Item * find(Table *t,int k1, char *k2){
     int i=0;
-    while(i<ptab->csize && (strcmp(ptab->ks2[i].key,k2))&&(ptab->ks1[i].key==k1))
+    while((i<t->csize) && (t->ks1[i].key!=k1)) {
         i++;
+    }
     printf("%d\n",i);
-    if(i==ptab->csize) return NULL;
-    else return ptab->ks1[i].info;
+    if(i==t->csize) return NULL;
+    else return t->ks1[i].info;
 }
 
-int insert(Table* ptab, int* k1, char *k2, char * info){
-    if(find(ptab,k1,k2)!=NULL) return 1;
+int insert(Table* t, int* k1, char *k2, char * info){
+    if(find(t,k1,k2)!=NULL) return 1;
     else{
-        if(ptab->msize==ptab->csize) return 2;
+        if(t->msize==t->csize) return 2;
         else{
             Item * item=malloc(sizeof(Item));
             item->key1=k1;
             item->key2=k2;
             item->inf=info;
-            ptab->ks1[ptab->csize].key=k1;
-            ptab->ks1[ptab->csize].info=item;
-            ptab->ks2[ptab->csize].key=k2;
-            ptab->ks2[ptab->csize].info=item;
-            ptab->csize++;
+            t->ks1[t->csize].key=k1;
+            t->ks1[t->csize].info=item;
+            t->ks2[t->csize].key=k2;
+            t->ks2[t->csize].info=item;
+            t->csize++;
             return 0;
         }
     }
 }
 
-int delete(Table *ptab, int k1, char *k2){
+int delete(Table *t, int k1, char *k2){
     int i=0;
-    while(i<ptab->csize && (strcmp(ptab->ks2[i].key,k2))&&(ptab->ks1[i].key==k1)){
+    while(i<t->csize && (strcmp(t->ks2[i].key,k2))&&(t->ks1[i].key==k1)){
         i++;
     }
-    if(i==ptab->csize) return 0;
+    if(i==t->csize) return 0;
     else{
-        free(ptab->ks1[i].info->inf);
-        free(ptab->ks1[i].info);
-        if(i!=ptab->csize-1){
-            ptab->ks1[i]=ptab->ks1[(ptab->csize-1)];
+        free(t->ks1[i].info->inf);
+        free(t->ks1[i].info);
+        if(i!=t->csize-1){
+            t->ks1[i]=t->ks1[(t->csize-1)];
         }
 
-        ptab->csize=ptab->csize-1;
+        t->csize=t->csize-1;
         return 1;
     }
 }
